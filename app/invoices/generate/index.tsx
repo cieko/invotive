ad8bb3302b6b628b56967b3ecd73043f8dc98156
Cieko <110197023+cieko@users.noreply.github.com>
2024-12-21 19:05:06 +0530

    input validation using hook form

--- app/invoices/generate/index.tsx
+++ app/invoices/generate/index.tsx
@@ -1,19 +1,63 @@
-import { Text } from 'react-native';
-import { Button } from '~/components/Button';
-import CustomTextInput from '~/components/CustomTextInput';
-
-import KeyboardAwareScrollView from '~/components/KeyboardAwareScrollView'
+import { FormProvider, useForm } from "react-hook-form";
+import { Text, View } from "react-native";
+import { Button } from "~/components/Button";
+import CustomTextInput from "~/components/CustomTextInput";
+import KeyboardAwareScrollView from "~/components/KeyboardAwareScrollView";
 
 export default function GenerateInvoice() {
+  const form = useForm();
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
   return (
     <KeyboardAwareScrollView>
-      <Text className='mb-5 text-2xl font-bold'>Sender Info</Text>
+      <FormProvider {...form}>
+        <Text className="mb-5 text-2xl font-bold">Sender Info</Text>
 
-      <CustomTextInput label='Name' placeholder='Enter your name' />
-      <CustomTextInput label='Address' multiline placeholder='Enter your address' />
-      <CustomTextInput label='Tax ID' placeholder='Enter your Tax ID' />
+        <View className="gap-2">
+          <CustomTextInput
+            name="name"
+            label="Name"
+            placeholder="Enter your name"
+            control={form.control}
+            rules={{
+              required: "Name is required!",
+              maxLength: { value: 50, message: "Name cannot exceed 50 characters!" },
+            }}
+          />
+          <CustomTextInput
+            name="address"
+            label="Address"
+            multiline
+            placeholder="Enter your address"
+            control={form.control}
+            rules={{
+              required: "Address is required!",
+            }}
+          />
+          <CustomTextInput
+            name="taxId"
+            label="Tax ID"
+            placeholder="Enter your Tax ID"
+            control={form.control}
+            rules={{
+              required: "Tax ID is required!",
+              pattern: {
+                value: /^[0-9]{9}$/,
+                message: "Tax ID must be a 9-digit number!",
+              },
+            }}
+          />
+        </View>
 
-      <Button title='Next' className='mt-auto' />
+        <Button
+          title="Next"
+          className="mt-auto"
+          onPress={form.handleSubmit(onSubmit)}
+        />
+      </FormProvider>
     </KeyboardAwareScrollView>
   );
 }
--- components/CustomTextInput.tsx
+++ components/CustomTextInput.tsx
@@ -1,17 +1,36 @@
+import { useController, Control, FieldValues, RegisterOptions } from "react-hook-form";
 import { Text, TextInput, TextInputProps, View } from "react-native";
 
 type CustomTextInputProps = {
-  label: string
+  label: string;
+  name: string;
+  control: Control<FieldValues>;
+  rules?: RegisterOptions;
 } & TextInputProps;
 
-export default function CustomTextInput({ label, ...props }: CustomTextInputProps) {
+export default function CustomTextInput({ label, name, control, rules, ...props }: CustomTextInputProps) {
+  const {
+    field: { onChange, onBlur, value },
+    fieldState: { error },
+  } = useController({
+    name,
+    control,
+    rules,
+  });
+
   return (
-    <View className="mb-4">
+    <View>
       <Text className="mb-2 text-lg">{label}</Text>
       <TextInput
         {...props}
+        value={value}
+        onChangeText={onChange}
         className={`rounded border border-gray-300 p-4 ${props.className}`}
+        onBlur={onBlur}
       />
+      {error?.message && (
+        <Text className="text-red-500 mt-2">{error.message}</Text>
+      )}
     </View>
-  )
+  );
 }
